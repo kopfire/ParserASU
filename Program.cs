2d25972d9178c92e5cbacdcbcac95dc570870480
Данила Шелудьков <kopfireorganisation@gmail.com>
2021-10-26 13:53:47 +0400

    new rep

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace ParserASU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"http://raspisanie.asu.edu.ru/student"; 
+            var links = driver.FindElements(By.XPath(
+                @".//div[@id='divBig']/div[@id='divLittle']/div[@class='fucl_div']/form/table/tbody/tr/td/select[@id='facul']/option"));
+            foreach (IWebElement link in links)
+            {
+                Console.WriteLine(link.Text + " - " + link.GetAttribute("value"));
+            }
+            Thread.Sleep(3000);
+        }
+    }
+}
